c4accd1a87b098f7c69064364e83b93810c6801a
Alxanquiell <87214172+Alxanquiell@users.noreply.github.com>
2022-03-16 03:01:34 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
-# task.py
-from multiprocessing.managers import BaseManager,Namespace
-from multiprocessing import Process, Queue, Manager
-import time
+from multiprocessing import Pool
 
 
-if __name__=="__main__":
-	que=Queue()
-	lista=["listaaaaa","lissss"]
-	BaseManager.register("zxc",callable=lambda:que)
-	BaseManager.register("lista_d",callable=lambda:lista)
-	m=BaseManager(address=("127.0.0.1",50000),authkey=b"1234567890")
-	m.get_server().serve_forever()
+def f(x):
+   for i in range(10000000000):
+       print(i)
+
+if __name__ == '__main__':
+    with Pool(10) as p:
+        p.map(f, [1, 2, 3])
+
